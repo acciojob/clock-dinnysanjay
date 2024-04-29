c000496d4257dce11c8424377417817fad232dce
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-29 20:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Get reference to the paragraph tag
+var timerElement = document.getElementById("timer");
+
+// Function to update date and time
+function updateTime() {
+    var currentDate = new Date();
+    timerElement.innerHTML = currentDate;
+}
+
+// Call updateTime function every 1000 milliseconds (1 second)
+setInterval(updateTime, 1000);
