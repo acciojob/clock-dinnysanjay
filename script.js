6b0dab30f8d14acd53ad0301cdfe38d5af702b89
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-29 20:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //your JS code here. If required.
-// Get reference to the paragraph tag
 var timerElement = document.getElementById("timer");
 
 // Function to update date and time
-let currentDate=new Date();
+let currentDate = new Date();
 setInterval(function(){
-	currentDate=new Date;
-	timerElememt.innerHTML=currentDate.toLocalString();
-},1000
-})
+    currentDate = new Date();
+    timerElement.innerHTML = currentDate.toLocaleString();
+}, 1000);
